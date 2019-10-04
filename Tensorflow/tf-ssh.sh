41f029091d60a7be8236c77239b441540e29c247
abel-leulseged <abelleulseged@gmail.com>
2019-10-04 16:12:47 -0700

    echo line

--- Tensorflow/tf-ssh.sh
+++ Tensorflow/tf-ssh.sh
@@ -1,5 +1,5 @@
 export ZONE="us-west1-b"
 export INSTANCE_NAME="abel-tf"
-echo 'Click here to open Jupyter Lab: http://localhost:8080/lab'
 gcloud compute instances start INSTANCE_NAME --zone=$ZONE 
+echo 'Click here to open Jupyter Lab: http://localhost:8080/lab'
 gcloud compute ssh -q --zone=$ZONE jupyter@$INSTANCE_NAME -- -L 8080:localhost:8080
